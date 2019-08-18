0fce7c77c1031f9962de8b15c01cd7abba53a1ad
MargaretAN9 <37722313+MargaretAN9@users.noreply.github.com>
2019-08-18 15:59:13 -0400

    Add files via upload

---
+++ microscopeModuleOption/Micro/Micro.js
@@ -0,0 +1,29 @@
+module.exports = exports = function(pixels, options){
+  var defaults = require('../../util/getDefaults.js')(require('./info.json'));
+
+  options.x = Number(options.x) || defaults.x;
+  options.y = Number(options.y) || defaults.y;
+  color = options.color || defaults.color;
+  color = color.substring(color.indexOf('(') + 1, color.length - 1); // extract only the values from rgba(_,_,_,_)
+  color = color.split(',');
+
+  for(var x = 0; x < pixels.shape[0]; x += options.x){
+    for(var y = 0 ; y < pixels.shape[1]; y++){
+      pixels.set(x, y, 0, color[0]);
+      pixels.set(x, y, 1, color[1]);
+      pixels.set(x, y, 2, color[2]);
+      //pixels.set(x, y, 3, color[3]);
+    }
+  }
+    
+  for(var y = 0; y < pixels.shape[1]; y += options.y){
+    for(var x = 0 ; x < pixels.shape[0]; x++){
+      pixels.set(x, y, 0, color[0]);
+      pixels.set(x, y, 1, color[1]);
+      pixels.set(x, y, 2, color[2]);
+      //pixels.set(x, y, 3, color[3]);
+    }
+  }
+
+  return pixels;
+};
